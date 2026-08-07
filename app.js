ccf508952c9dfa8835c5faa8b395d9ec4911f878
nisar-khokhar <khokharmaliknisar@gmail.com>
2026-08-07 14:38:33 +0500

    07-08-2026 / First Commit -- created express project using express-generator, deleted views folder and installed nodemon as a dev dependency

---
+++ app.js
@@ -0,0 +1,81 @@
+/*
+
+* Author: Malik Nisar Jamil
+* Email: khokharmaliknisar@gmail.com
+
+
+_________________________________________________________________
+* Date: Fri Aug 07 2026
+            
+*/
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+/**
+ * Set the "dev" script in package.json to get data from .env.local file
+ * and install dotenv package to use this feature.
+ * This will help to set the PORT variable in .env.local file
+ * and use it in the app.js file.
+ *
+ */
+require("dotenv").config({
+  path: process.env.NODE_ENV === "development" ? ".env.local" : ".env",
+});
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+/**
+ * morgan logger logs the request type, path, status code, response time, and other details to the console for debugging and monitoring purposes.
+ *
+ */
+app.use(logger("dev"));
+
+/**
+ * This middleware parses incoming JSON request bodies.
+ *
+ */
+app.use(express.json());
+
+/**
+ * This parses data submitted from HTML forms.
+ *
+ */
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * parses cookies attached to the client request object and makes them available under req.cookies.
+ *
+ */
+app.use(cookieParser());
+
+/**
+ * If a request matches a file inside the public folder, return that file directly instead of executing any route.
+ *
+ */
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  res.json({
+    success: false,
+    message: err.message,
+  });
+});
+
+module.exports = app;
